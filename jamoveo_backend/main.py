8adb50de4c0b64f283e4f4a07275c4c982e8eca3
nirpr <nirpr@mta.ac.il>
2025-03-13 14:42:51 +0200

    debug change

--- jamoveo_backend/main.py
+++ jamoveo_backend/main.py
@@ -14,10 +14,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  # Local development
-        "https://jamoveo.vercel.app"  # Production frontend
-    ],
+    # allow_origins=[
+    #     "http://localhost:3000",  # Local development
+    #     "https://jamoveo.vercel.app"  # Production frontend
+    # ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
